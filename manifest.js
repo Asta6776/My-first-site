d103ed272a10939fbf92b3d30cfb5f96b28b98ec
Asta6776 <rodericklibres5@gmail.com>
2026-04-25 22:42:52 +0800

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,15 @@
+{
+  "name": "Eco-Saver Pro",
+  "short_name": "EcoSaver",
+  "start_url": ".",
+  "display": "standalone",
+  "background_color": "#0d2218",
+  "theme_color": "#1a3a2a",
+  "icons": [
+    {
+      "src": "https://img.icons8.com/fluency/192/leaf.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ]
+}
